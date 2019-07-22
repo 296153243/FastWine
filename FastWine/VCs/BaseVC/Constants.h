485ba597c65ec2756ced467b13ccabcd923ac09a
296153243 <296153243@qq.com>
2019-07-22 10:11:00 +0800

    删除多余图片资源

--- FastWine/VCs/BaseVC/Constants.h
+++ FastWine/VCs/BaseVC/Constants.h
@@ -76,8 +76,8 @@
 #ifdef DEBUG
 //#define HOSTNAME @"http://mall.jiudicar.com/"//正式
 //#define HOSTNAME @"http://wine.jiudi.cn/"//本地api
-#define HOSTNAME @"http://test.jiudicar.com/"//test
-//#define HOSTNAME @"http://wine.jiudicar.com/"//正式
+//#define HOSTNAME @"http://test.jiudicar.com/"//test
+#define HOSTNAME @"http://wine.jiudicar.com/"//正式
 
 
 
